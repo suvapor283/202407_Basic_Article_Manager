bf77b0c12a5dbc15b6779efa886e0552b79275b3
suvapor283 <suvapor8632@gmail.com>
2024-07-15 21:23:12 +0900

    article list

---
+++ src/Article.java
@@ -0,0 +1,14 @@
+
+public class Article {
+
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body){
+		
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +8,9 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
+
+		List<Article> articles = new ArrayList<>();
+
 		int lastArticleId = 0;
 
 		while (true) {
@@ -16,24 +21,38 @@ public class Main {
 			if (cmd.equals("exit")) {
 				break;
 			}
-			
+
 			if (cmd.isEmpty()) {
 				System.out.println("명령어를 입력해주세요.");
 				continue;
 			}
-			
+
 			if (cmd.equals("article write")) {
-   
+
 				System.out.print("제목 : ");
 				String title = sc.nextLine().trim();
 				System.out.print("내용 : ");
 				String body = sc.nextLine().trim();
 
-				System.out.println(++lastArticleId + "번 글이 생성되었습니다.");
+				lastArticleId++;
+				articles.add(new Article(lastArticleId, title, body));
+
+				System.out.println(lastArticleId + "번 글이 생성되었습니다.");
 			}
-			
+
 			else if (cmd.equals("article list")) {
-				System.out.println("게시물이 존재하지 않습니다.");
+
+				if (articles.isEmpty()) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
 			}
 
 			else {
