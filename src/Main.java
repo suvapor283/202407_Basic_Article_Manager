bbede00b58cd08092c26de61077cd4c55bd3eb40
suvapor283 <suvapor8632@gmail.com>
2024-07-16 20:05:22 +0900

    article detail

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,6 @@ public class Main {
 			}
 
 			if (cmd.equals("article write")) {
-
 				System.out.print("제목 : ");
 				String title = sc.nextLine().trim();
 				System.out.print("내용 : ");
@@ -41,38 +40,40 @@ public class Main {
 			}
 
 			else if (cmd.equals("article list")) {
-
 				if (articles.isEmpty()) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-				
+
 				System.out.println("번호	|	제목");
-				
+
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
 			}
-			
+
 			else if (cmd.startsWith("article detail ")) {
-				String[] a = cmd.split(" ");
-				int chkNum = Integer.parseInt(a[2]);
-				
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
 				for (Article article : articles) {
-					if (chkNum != article.id) {
-						System.out.printf("%d번 게시물은 존재하지 않습니다.", chkNum);
+					if (id == article.id) {
+						foundArticle = article;
 						break;
 					}
 				}
-				
-				for (Article article : articles) {
-					if (chkNum == article.id) {
-						System.out.println("번호 : " + article.id);
-						System.out.println("제목 : " + article.title);
-						System.out.println("내용 : " + article.body);
-					}
+
+				if (foundArticle == null) {
+					System.out.println(id + "번 게시물은 존재하지 않습니다.");
+					continue;
 				}
+
+				System.out.println("번호 : " + foundArticle.id);
+				System.out.println("제목 : " + foundArticle.title);
+				System.out.println("내용 : " + foundArticle.body);
 			}
 
 			else {
