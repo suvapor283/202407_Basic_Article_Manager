3e8ae9663dbd477b98aaa45f4468ea26bba2e2ff
suvapor283 <suvapor8632@gmail.com>
2024-07-23 19:11:54 +0900

    MemberController

--- src/com/exam/BAM/app/App.java
+++ src/com/exam/BAM/app/App.java
@@ -4,29 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.exam.BAM.controller.MemberController;
 import com.exam.BAM.dto.Article;
-import com.exam.BAM.dto.Member;
 import com.exam.BAM.util.Util;
 
 public class App {
 	private List<Article> articles;
-	private List<Member> members;
 	private int lastArticleId;
-	private int lastMemberId;
 
 	public App() {
 		this.articles = new ArrayList<>();
-		this.members = new ArrayList<>();
 		this.lastArticleId = 0;
-		this.lastMemberId = 0;
 	}
 	
 	public void run() {
 		System.out.println("== 프로그램 시작 ==");
 
-		makeTestData();
-
 		Scanner sc = new Scanner(System.in);
+		MemberController memberController = new MemberController(sc);
+		
+		memberController.makeTestData();
+		makeTestData();
 
 		while (true) {
 
@@ -43,68 +41,7 @@ public class App {
 			}
 			
 			if (cmd.equals("member join")) {
-				String loginId = null;
-				String loginPw = null;
-				String name = null;
-				
-				while (true) {
-					System.out.print("아이디 : ");
-					loginId = sc.nextLine().trim();
-
-					if (loginId.isEmpty()) {
-						System.out.println("필수 입력 정보입니다.");
-						continue;
-					}
-					
-					boolean isLoginIdDup = false;
-					
-					for (Member member : members) {
-						if (loginId.equals(member.getLoginId())) {
-							isLoginIdDup = true;
-							break;
-						}
-					}
-					if (isLoginIdDup) {
-						System.out.printf("[ %s ]는 이미 존재하는 아이디입니다 !!\n", loginId);
-						continue;
-					}
-					
-					System.out.printf("[ %s ]는 사용가능한 아이디입니다 !!\n", loginId);
-					break;
-				}
-				
-				while (true) {
-					System.out.print("비밀번호 : ");
-					loginPw = sc.nextLine().trim();
-					
-					if (loginPw.isEmpty()) {
-						System.out.println("필수 입력 정보입니다.");
-						continue;
-					}
-					
-					System.out.print("비밀번호 확인 : ");
-					String chkLoginPw = sc.nextLine().trim();
-					
-					if (chkLoginPw.equals(loginPw) == false) {
-						System.out.println("비밀번호가 일치하지 않습니다.");
-						continue;
-					}
-					break;
-				}
-				
-				while (true) {
-					System.out.print("이름 : ");
-					name = sc.nextLine().trim();
-					
-					if (name.isEmpty()) {
-						System.out.println("필수 입력 정보입니다.");
-						continue;
-					}
-					break;
-				}
-				
-				members.add(new Member(++lastMemberId, Util.getDateStr(), loginId, loginPw, name));
-				System.out.printf("[ %s ]님 회원가입을 축하합니다 !!\n", name);
+				memberController.dojoin();
 			}
 
 			else if (cmd.equals("article write")) {
@@ -250,7 +187,6 @@ public class App {
 		System.out.println("테스트 게시물 데이터와 회원 데이터를 생성했습니다!");
 		for (int i = 1; i < 4; i++) {
 			articles.add(new Article(++lastArticleId, Util.getDateStr(), (i + "번"), (i + "번 게시물 내용"), (i * 10)));
-			members.add(new Member(++lastMemberId, Util.getDateStr(), ("user" + i), ("user" + i), ("유저" + i)));
 		}
 	}
 }
---
+++ src/com/exam/BAM/controller/MemberController.java
@@ -0,0 +1,92 @@
+package com.exam.BAM.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.exam.BAM.dto.Member;
+import com.exam.BAM.util.Util;
+
+public class MemberController {
+	private Scanner sc;
+	private List<Member> members;
+	private int lastMemberId;
+
+	public MemberController(Scanner sc) {
+		this.sc = sc;
+		this.members = new ArrayList<>();
+		this.lastMemberId = 0;
+	}
+
+	public void dojoin() {
+		String loginId = null;
+		String loginPw = null;
+		String name = null;
+
+		while (true) {
+			System.out.print("아이디 : ");
+			loginId = sc.nextLine().trim();
+
+			if (loginId.isEmpty()) {
+				System.out.println("필수 입력 정보입니다.");
+				continue;
+			}
+
+			boolean isLoginIdDup = false;
+
+			for (Member member : members) {
+				if (loginId.equals(member.getLoginId())) {
+					isLoginIdDup = true;
+					break;
+				}
+			}
+			if (isLoginIdDup) {
+				System.out.printf("[ %s ]는 이미 존재하는 아이디입니다 !!\n", loginId);
+				continue;
+			}
+
+			System.out.printf("[ %s ]는 사용가능한 아이디입니다 !!\n", loginId);
+			break;
+		}
+
+		while (true) {
+			System.out.print("비밀번호 : ");
+			loginPw = sc.nextLine().trim();
+
+			if (loginPw.isEmpty()) {
+				System.out.println("필수 입력 정보입니다.");
+				continue;
+			}
+
+			System.out.print("비밀번호 확인 : ");
+			String chkLoginPw = sc.nextLine().trim();
+
+			if (chkLoginPw.equals(loginPw) == false) {
+				System.out.println("비밀번호가 일치하지 않습니다.");
+				continue;
+			}
+			break;
+		}
+
+		while (true) {
+			System.out.print("이름 : ");
+			name = sc.nextLine().trim();
+
+			if (name.isEmpty()) {
+				System.out.println("필수 입력 정보입니다.");
+				continue;
+			}
+			break;
+		}
+
+		members.add(new Member(++lastMemberId, Util.getDateStr(), loginId, loginPw, name));
+		System.out.printf("[ %s ]님 회원가입을 축하합니다 !!\n", name);
+	}
+
+	public void makeTestData() {
+		System.out.println("테스트용 회원 데이터 3개를 생성했습니다!");
+		for (int i = 1; i <= 3; i++) {
+			members.add(new Member(++lastMemberId, Util.getDateStr(), ("user" + i), ("user" + i), ("유저" + i)));
+		}
+	}
+}
