8e468ee7d5fcd00eb437eaee2778a0cfa6f7c015
suvapor283 <suvapor8632@gmail.com>
2024-07-24 19:12:02 +0900

    Controller

--- src/com/exam/BAM/app/App.java
+++ src/com/exam/BAM/app/App.java
@@ -38,7 +38,7 @@ public class App {
 			else if (cmd.equals("article write")) {
 				articleController.doWrite();
 			}
-
+			
 			else if (cmd.startsWith("article list")) {
 				articleController.showList(cmd);
 			}
